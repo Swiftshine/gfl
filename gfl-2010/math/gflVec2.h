7619001eeaba3cbcb74f0a53cd91d6a5713459a3
Swift <swiftshineexists@gmail.com>
2024-08-12 20:39:56 -0500

    add gfl::Vec2 and gfl::Vec3 to 2010 folder

---
+++ gfl-2010/math/gflVec2.h
@@ -0,0 +1,11 @@
+#ifndef GFL_MATH_VEC2_H
+#define GFL_MATH_VEC2_H
+
+namespace gfl {
+    class Vec2 {
+    public:
+        float x, y;
+    };
+}
+
+#endif
---
+++ gfl-2010/math/gflVec3.h
@@ -0,0 +1,11 @@
+#ifndef GFL_MATH_VEC3_H
+#define GFL_MATH_VEC3_H
+
+namespace gfl {
+    class Vec3 {
+    public:
+        float x, y, z;
+    };
+}
+
+#endif
